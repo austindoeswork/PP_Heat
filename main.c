a1deb5f8b1d8a451a944d43d04df07c3c2aae2e0
aidan <aidanwenzel@gmail.com>
2017-04-27 21:35:40 -0400

    Create CMakeLists.txt and a main.c file

--- main.c
+++ main.c
@@ -15,13 +15,34 @@
 /* Define Macros *****************************************/
 /*********************************************************/
 
+#define TICK 1 //length of time for a tick
+
 /*********************************************************/
 /* Global Vaiable Definitions ****************************/
 /*********************************************************/
 
+float* universe, universeNext; //X,Y,Z
+
+int worldsize, myrank, aboveRank, belowRank;
+int numTicks;
+int dimX, dimY, dimZ; //Dimensions of board
+
 /*********************************************************/
 /* Function Definitions **********************************/
 /*********************************************************/
+void printUniverse();
+
+float* emptyUniverse(){
+    return calloc((size_t)dimX*dimY*dimZ, sizeof(float));
+}
+
+void initializeUniverse(char* filename){
+    universe = (float*) calloc((size_t)dimX*dimY*dimZ, sizeof(float));
+
+    //TODO: write the read file to read in objects into grid
+}
+
+void tick();
 
 /*********************************************************/
 /* Function Main *****************************************/
